3e356341ee602bc2838a9f7653cb5a391fef617b
Cosme Zamudio <cosmez@gmail.com>
2014-10-22 21:30:25 -0600

    Done with page 28, started page 29

--- 01.rkt
+++ 01.rkt
@@ -119,7 +119,7 @@
   (product-1 sos1 sos2))
 
 ;;page 27
-;;filter-in Predicate X ListOf(SchemeVal) -> ListOf(SchemeVal)
+;;filter-in Function X ListOf(SchemeVal) -> ListOf(SchemeVal)
 (define (filter-in pred? lst)
   (if (null? lst) lst
       (if (pred? (car lst))
@@ -127,10 +127,96 @@
           (filter-in pred? (cdr lst)))))
 
 ;;page 27
-;;list-index Predicate X ListOf(SchemeVal) -> Number
+;;list-index Function X ListOf(SchemeVal) -> Number
 (define (list-index pred? lst)
   (define (list-index-counter pred? lst i)
     (if (null? lst) #f
      (if (pred? (car lst)) i 
          (list-index-counter pred? (cdr lst) (+ i 1)))))
   (list-index-counter pred? lst 0))
+
+;;page 28
+;;every? Function X ListOf(Symbol) -> Boolean
+(define (every? pred? lst)
+  (if (null? lst) #t
+      (and (pred? (car lst)) (every? pred? (cdr lst)))))
+
+;;page 28
+;;every? Function X ListOf(Symbol) -> Boolean
+(define (exists? pred? lst)
+  (if (null? lst) #f
+      (or (pred? (car lst)) (exists? pred? (cdr lst)))))
+
+;;page 28
+;;up ListOf(List) -> ListOf(List)
+(define (up lst)
+  (define (up-1 lst-car lst-rest)
+    (if (null? lst-car) 
+        (up lst-rest)
+        (cons (car lst-car) (up-1 (cdr lst-car) lst-rest))))
+  (cond 
+    [(null? lst) lst]
+    [(list? (car lst)) (up-1 (car lst) (cdr lst))]
+    [else (cons (car lst) (up (cdr lst))) ]))
+
+;;page 28
+;;flatten list? -> list?
+(define (flatten lst)
+  (if (every? (lambda (el) (not (list? el))) lst) lst
+      (flatten (up lst))))
+
+;;page 28
+;;merge ListOf(Integer) x ListOf(Integer) -> ListOf(Integer)
+(define (merge loi1 loi2)
+  (if (null? loi1) loi2
+      (cons (car loi1) (merge (cdr loi1) loi2))))
+
+;;remove-at ListOf(SchemeVal) x Integer -> ListOf(SchemeVal)
+(define (remove-at lst index)
+  (define (remove-1 lst i)
+    (cond
+      [(null? lst) lst]
+      [(= index i) (cdr lst)]
+      [else (cons (car lst) (remove-1 (cdr lst) (+ i 1)))]))
+  (remove-1 lst 0))
+
+
+;;page 28
+;;sort ListOf(Integer) -> ListOf(Integer)
+(define (sort integer-list)
+  (define (partition pivot lst tmp1 tmp2)
+    (cond 
+      [(null? lst) (list tmp1 tmp2)]
+      [(< (car lst) pivot) (partition pivot (cdr lst) (cons (car lst) tmp1) tmp2)]
+      [else (partition pivot (cdr lst) tmp1 (cons (car lst) tmp2))]))
+  (if (or (null? integer-list) 
+          (eqv? 1 (list-length integer-list))) 
+      integer-list
+      (let* ([pivot (list-ref integer-list (floor (/ (list-length integer-list) 2)))]
+            [pivotless-list (remove-at integer-list (floor (/ (list-length integer-list) 2)))]
+            [result (partition pivot pivotless-list '() '())])
+        (merge (merge (sort (car result)) (list pivot)) (sort (cadr result))))))
+
+;;page 29
+;;sorted-merge ListOf(Integer) x ListOf(Integer) -> ListOf(Integer)
+(define (sorted-merge loi1 loi2)
+  (sort (merge loi1 loi2)))
+
+;;page 29
+;;sort ListOf(Integer) x Function -> ListOf(Integer)
+(define (sort/predicate integer-list pred?)
+  (define (partition pivot lst tmp1 tmp2)
+    (cond 
+      [(null? lst) (list tmp1 tmp2)]
+      [(pred? (car lst) pivot) (partition pivot (cdr lst) (cons (car lst) tmp1) tmp2)]
+      [else (partition pivot (cdr lst) tmp1 (cons (car lst) tmp2))]))
+  (if (or (null? integer-list) 
+          (eqv? 1 (list-length integer-list))) 
+      integer-list
+      (let* ([pivot (list-ref integer-list (floor (/ (list-length integer-list) 2)))]
+            [pivotless-list (remove-at integer-list (floor (/ (list-length integer-list) 2)))]
+            [result (partition pivot pivotless-list '() '())])
+        (merge (merge (sort/predicate (car result) pred?) (list pivot)) 
+               (sort/predicate (cadr result) pred?)))))
+
+
