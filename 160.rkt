fc34f8deffdccf54a662f5700baa441e26d40df9
Cosme Zamudio <cosmez@gmail.com>
2015-01-15 00:08:27 -0700

    Delete 160.rkt

--- 160.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(define sine (compose sin degrees->radians))
-(define cosine (compose cos degrees->radians))
-(define tangent (compose tan degrees->radians))
-(define asine (compose radians->degrees asin))
-(define acosine (compose radians->degrees acos))
-(define atangent (compose radians->degrees atan))
